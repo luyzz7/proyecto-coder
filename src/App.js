0e8770af3e8f427db51481b25339565a9ff979e1
Diego Martegani <diego.martegani@fing.edu.uy>
2022-09-10 16:41:23 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Fabian se la come
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
